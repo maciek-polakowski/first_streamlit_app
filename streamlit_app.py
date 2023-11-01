7dd4c05c02b66f6d17743d9c7c3f0cdedcba03ad
MACIEJ POLAKOWSKI <maciek.polakowski@gmail.com>
2023-11-01 16:44:26 +0100

    Update streamlit_app.py
    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Bluberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothy')
+streamlit.text('Hard-boiled free-range Eggs')
+
