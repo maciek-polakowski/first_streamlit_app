85cb7ded9d209c027b957c5ba6760e5689fcc5c8
MACIEJ POLAKOWSKI <maciek.polakowski@gmail.com>
2023-11-01 16:47:49 +0100

    Update streamlit_app.py
    4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My Mom's new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
    
