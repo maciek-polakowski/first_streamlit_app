549259c2fa6836d946206aeb245fd11369e6f756
MACIEJ POLAKOWSKI <maciek.polakowski@gmail.com>
2023-11-04 15:12:23 +0100

    7.7

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,13 +43,16 @@ except URLError as e:
   streamlit.error()
 
 
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:  
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+
+
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+streamlit.dataframe(get_fruit_load_list())
 
 streamlit.header("Fruityvice Fruit Advice!")
 
