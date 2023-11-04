130350388d79deaf0ed2f00a82a15072a913154f
MACIEJ POLAKOWSKI <maciek.polakowski@gmail.com>
2023-11-04 15:29:11 +0100

    7.9

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.text("The fruit load list contains:")
+streamlit.text("View Our Fruit List - Add Your Favorites!")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 def get_fruit_load_list():
@@ -51,7 +51,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   streamlit.dataframe(get_fruit_load_list())
 
 streamlit.header("Fruityvice Fruit Advice!")
